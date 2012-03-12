02e9b045d4915da27d8137414b3f0fddcb680a26
Ash McKenzie <ash.mckenzie@playup.com>
2012-03-13 10:08:58 +1100

    cron timing now in config.yml

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,11 @@
 require 'yaml'
+require 'pry'
 
 CONFIG = YAML.load_file('config/config.yml')
 
 set :base, "#{CONFIG['base_location']}/current"
 set :output, "#{base}/log/cron.log"
 
-every 1.day, :at => '8:00 pm' do
+send(:every, eval(CONFIG['app']['cron']['frequency']), eval("{ #{CONFIG['app']['cron']['options']} }")) do
   command "cd #{base} && ./reddit-emailer --limit #{CONFIG['app']['limit']} --subreddit #{CONFIG['app']['subreddit']} --email \"#{CONFIG['app']['emails']}\""
 end
