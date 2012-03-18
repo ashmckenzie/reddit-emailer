a0fc272615676af823bbe98816a74c43e98e89b6
Ash McKenzie <ash.mckenzie@playup.com>
2012-03-19 09:19:17 +1100

    oops

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,5 +6,5 @@ set :base, "#{ENV['HOME']}/#{CONFIG['app']['name']}/current"
 set :output, "#{base}/log/cron.log"
 
 send(:every, eval(CONFIG['app']['cron']['frequency']), eval("{ #{CONFIG['app']['cron']['options']} }")) do
-  command "cd #{base} && ./reddit-emailer --limit #{CONFIG['app']['reddit']['limit']} --subreddit #{CONFIG['app']['subreddit']} --email \"#{CONFIG['app']['email']['to']}\""
+  command "cd #{base} && ./reddit-emailer --limit #{CONFIG['app']['reddit']['limit']} --subreddit #{CONFIG['app']['reddit']['subreddit']} --email \"#{CONFIG['app']['email']['to']}\""
 end
