a859d8e40e4797165b3b9b08e04cb81e2946eb1e
Ash McKenzie <ash@greenworm.com.au>
2012-04-13 07:58:22 +1000

    cleanup

---
+++ lib/reddit/emailer.rb
@@ -0,0 +1,99 @@
+require 'erb'
+require 'json'
+require 'open-uri'
+
+require_relative 'story'
+
+module Reddit
+  class Emailer
+
+    def initialize subreddit, limit, email_list
+      @stories = []
+
+      @subreddit = subreddit
+      @email_list = email_list
+      @limit = limit
+      @response = JSON.parse(RestClient.get(url, { 'Cache-Control' => 'no-cache' }))
+
+      process_response
+
+      unless $DEBUG_ON
+        send_email
+      else
+        ap generate_html
+      end
+    end
+
+    private
+
+    def url
+      @url ||= $CONFIG['reddit']['url'] % { :subreddit => @subreddit }
+    end
+
+    def process_response
+      @response['data']['children'].each do |json|
+        story = Reddit::Story.new(json)
+        @stories << story unless story.image_urls.empty?
+        return if @stories.size == @limit
+      end
+    end
+
+    def email_subject
+      @email_subject ||= "%{title} for %{now}" % { :title => title, :now => now }
+    end
+
+    def send_email
+      body = InlineStyle.process(generate_html, :stylesheets_path => "./lib/templates/styles")
+
+      mail = Mail.new
+      mail.from = $CONFIG['email']['from']
+      mail.to = @email_list
+      mail.subject = email_subject
+      mail.html_part do
+        content_type  'text/html; charset=UTF-8'
+        body body
+      end
+
+      debug_log(mail.to_s)
+
+      unless $DRY_ON
+        mail.delivery_method :sendmail
+        mail.deliver!
+      else
+        require 'tempfile'
+        filename = Tempfile.new(['reddit-emailer', '.html'])
+        File.open(filename, 'w') { |f| f.write(body) }
+        `open #{filename.path}`
+        sleep(2)
+      end
+    end
+
+    def now
+      Time.now.strftime('%A %d %B %Y')
+    end
+
+    def title
+      "Top %{limit} images for Reddit '%{subreddit}'" % { :limit => @limit, :subreddit => @subreddit }
+    end
+
+    def generate_html
+      body = generate_body_html
+      email_template = ERB.new(File.read('./lib/templates/email.html.erb'))
+      email_template.result(binding)
+    end
+
+    def generate_body_html
+      stories_html = []
+      @stories.each do |story|
+        stories_html << generate_story_html(story)
+      end
+      stories_html.join("\n")
+    end
+
+    def generate_story_html story
+      @story_template ||= ERB.new(File.read('./lib/templates/shared/_story.html.erb'))
+      @story_template.result(binding)
+    end
+
+  end
+end
---
+++ lib/reddit/story.rb
@@ -0,0 +1,49 @@
+require 'open-uri'
+require 'cgi'
+
+module Reddit
+  class Story
+
+    IMGUR_DOMAIN = 'imgur.com'
+
+    def initialize json
+      @json = json
+    end
+
+    def domain
+      @json['data']['domain']
+    end
+
+    def url
+      @json['data']['url']
+    end
+
+    def image_urls
+
+      images = []
+
+      if url.match(/\.(jpg|jpeg|gif|png)$/i)
+        images = [ url ]
+      elsif domain == IMGUR_DOMAIN
+
+        # Lets try and extract out the imgur.com image :)
+        #
+        images = []
+
+        begin
+          images = Nokogiri::HTML(open(url)).search('div.left div.panel img').collect { |image| (image.attribute('data-src') || image.attribute('src')).to_s }
+        rescue SocketError => e
+          false
+        end
+      end
+
+      images.collect do |image|
+        "#{$CONFIG['image_scaler']['url']}/#{$CONFIG['image_scaler']['dimensions']}/#{CGI.escape(image)}"
+      end
+    end
+
+    def method_missing(meth, *args, &block)
+      @json['data'][meth.to_s]
+    end
+  end
+end
