20c2207290c4191d5037ee3e2f5ab3acdeaa5181
Ash McKenzie <ash@greenworm.com.au>
2014-01-25 15:11:23 +1100

    Fixes

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,17 +4,15 @@ def config
   RedditEmailer::Config.instance
 end
 
-set :base, "#{ENV['HOME']}/#{config.name}/current"
+set :base, "#{ENV['HOME']}/#{config.name}"
 set :output, "#{base}/log/cron.log"
-
-require 'pry'
 
 config.email.to.each do |x|
   run_at, emails = x
   run_at = run_at.gsub(/_/, ':')
 
   set :cmd, "cd #{base} && \
-  ERRBIT_ENABLE=true APP_ENV=production ./bin/reddit-emailer \
+  ERRBIT_ENABLE=true APP_ENV=production bundle exec ./bin/reddit-emailer \
   --subreddit #{config.reddit.subreddit} \
   --emails \"#{emails}\""
 
