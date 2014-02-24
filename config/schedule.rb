734aec1f07f8a4fcfabd68adfe77faf01a3174d9
Ash McKenzie <ash@greenworm.com.au>
2014-02-24 23:02:33 +1100

    Duh.

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,12 +8,12 @@ env :PATH, "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 config.email.to.each do |x|
 
-  set :command, %Q{cd #{base} && \
+  set :cmd, %Q{cd #{base} && \
    ERRBIT_ENABLE="true" APP_ENV="production" /usr/local/bin/bundle exec ./bin/reddit-emailer \
    --maximum #{x.maximum} \
    --subreddit #{config.reddit.subreddit} \
    #{x.options} \
    --emails "#{x.recipients}"}
 
-  send(:every, 1.day, at: x.time) { command(command) }
+  send(:every, 1.day, at: x.time) { command(cmd) }
 end
