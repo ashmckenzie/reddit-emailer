5b82ff44e32801a55495dd0572ab62f37139bb56
Ash McKenzie <ash@greenworm.com.au>
2014-03-07 10:35:22 +1100

    Recipients are now an Array in config.yml

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,12 +8,14 @@ env :PATH, "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 config.email.to.each do |x|
 
+  recipients = x.recipients.join(', ')
+
   set :cmd, %Q{cd #{base} && \
-   ERRBIT_ENABLE="true" APP_ENV="production" /usr/local/bin/bundle exec ./bin/reddit-emailer \
-   --maximum #{x.maximum} \
-   --subreddit #{config.reddit.subreddit} \
-   #{x.options} \
-   --emails "#{x.recipients}"}
+    APP_ENV="production" /usr/local/bin/bundle exec ./bin/reddit-emailer \
+    --maximum #{x.maximum} \
+    --subreddit #{config.reddit.subreddit} \
+    #{x.options} \
+    --emails "#{recipients}"}
 
   send(:every, 1.day, at: x.time) { command(cmd) }
 end
